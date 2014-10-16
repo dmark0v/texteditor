991aca2afcfc4f15b2424acf5e2ec5af1bce9666
mrUnkn0wn <mr.unkn0wn@aol.com>
2014-10-16 20:41:02 +0400

    first button

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var texteditor = function(){
+    this.$el = $('#content');
+    $('.te-btn-bold').on('click',function(e,b,c){
+        this.$el.append('<strong>a</strong>');
+        var range = document.createRange();
+        var sel = window.getSelection();
+        debugger;
+        range.setStart(this.$el.find('strong')[0],8);
+        range.setEnd(this.$el.find('strong')[0],8);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }.bind(this));
+    return this;
+};
 
 
